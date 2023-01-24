c0b1c6dd85483fb6d5f5a11349d387dcec80854d
Nathálya Ricci <nathalyaricci@gmail.com>
2023-01-24 17:49:06 -0300

    finaliza base

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,43 @@
 import "./App.css";
+import { useState } from "react";
+import { Avatar } from "./components/Avatar/Avatar";
+import { Header } from "./components/Header/Header";
 import ButtonLink from "./components/Buttons/Buttons";
 import { ButtonSwitcher } from "./components/Buttons/Buttons";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
 	return (
 		<div className="App">
-			<ButtonLink
-				socialLink={"https://discord.com/users/506557042530451482"}
-				socialName="Discord"
+			<Avatar />
+			<Header
+				name="Nathálya Ricci"
+				career="Frontend Developer"
+				techs="HTML | CSS | JAVASCRIPT"
 			/>
-			<ButtonSwitcher />
+			<div className="buttons-wrapper">
+				<ButtonLink
+					socialLink={"https://discord.com/users/506557042530451482"}
+					socialIcon="./images/discord.svg"
+					socialName="Discord"
+				/>
+				<ButtonLink
+					socialLink={"https://github.com/nathalya-ricci"}
+					socialIcon="./images/github.svg"
+					socialName="Github"
+				/>
+				<ButtonLink
+					socialLink={"https://linkedin.com/in/nathalya-ricci"}
+					socialIcon="./images/linkedin.svg"
+					socialName="Linkedin"
+				/>
+			</div>
+			<div className="ButtonSwitcher">
+				<ButtonSwitcher text="Mudar tema" />
+			</div>
+			<div className="Footer">
+				<Footer text="Feito com 🖤 por Nathálya Ricci" />
+			</div>
 		</div>
 	);
 }
