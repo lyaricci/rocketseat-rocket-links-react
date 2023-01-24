94b8cf3d6e2deeb69febf1c481e033f9b8c57192
Nathálya Ricci <nathalyaricci@gmail.com>
2023-01-24 10:46:14 -0300

    atualiza caminho da imagem do Avatar

--- src/App/components/Avatar/Avatar.jsx
+++ src/App/components/Avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import "./Avatar.css";
 export function Avatar() {
   return (
     <div>
-      <img className="Avatar" src="./public/images/lya.jpg" alt="Meu avatar" />
+      <img className="Avatar" src="./images/lya.jpg" alt="Meu avatar" />
     </div>
   )
 }
