ee7c048e3164e76b304ee24e4daaeea7b7960fc7
Nathálya Ricci <nathalyaricci@gmail.com>
2023-01-24 13:39:29 -0300

    cria componente Header.jsx

---
+++ src/App/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export function Header({name, career, techs}) {
+  return (
+    <div className="Header">
+      <h1>{name}</h1>
+      <p>{career}</p>
+      <p>{techs}</p>
+    </div>
+  )
+}
